f43cac0150de3ffaa58d0ca888be45c1ef5c3f18
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-21 19:36:24 +0530

    user model

---
+++ src/db/models/user.js
@@ -0,0 +1,55 @@
+const Sequelize = require("sequelize");
+const bcrypt = require("bcrypt");
+const config = require("../config/config.js");
+const connection = require("../config/connection.js");
+const sequelize = connection.sequelize;
+
+const User = sequelize.define("users", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: null,
+    primaryKey: false,
+    unique: false,
+  },
+  pass: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: null,
+    primaryKey: false,
+    unique: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: null,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  admin: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+    primaryKey: false,
+    unique: false,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: null,
+    primaryKey: false,
+    unique: false,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: null,
+    primaryKey: false,
+    unique: false,
+  },
+});
+sequelize.sync();
+
+module.exports = { User };
