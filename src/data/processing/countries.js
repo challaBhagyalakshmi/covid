c1fc40af0098342a74de38cb3403d0ce65811417
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-21 20:14:04 +0530

    export countries to csv to countries table

---
+++ src/data/processing/countries.js
@@ -0,0 +1,29 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+const countries = require("../../db/Models/countries.js");
+
+const Country = countries.country;
+
+async function countries_data() {
+  fs.createReadStream("../csv_files/countries.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+      const data = Object.values(row);
+      const dat = data[0];
+      Country.sync()
+        .then(function () {
+          return Country.create({
+            country_name: dat,
+          });
+        })
+        .then((data) => {
+          console.log(data.country_name);
+        });
+    })
+    .on("end", () => {
+      console.log("csv file successfully processed");
+    });
+}
+
+countries_data();
+module.exports = { countries_data };
