281c77981023c07dd90501719d5a2f2aaa6d6157
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-21 19:34:13 +0530

    configuration file

---
+++ src/db/config/config.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+Credential = {
+  dbname: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  testdb: process.env.test_db,
+  admin_pass: process.env.admin_pass,
+  api_key: process.env.secret_key,
+};
+
+module.exports = { Credential };
