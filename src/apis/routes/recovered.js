3185686577d11559f2511393ee005fed1fa14fa4
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-23 01:16:42 +0530

    top10 countries who is having highest covid recovered cases in world

---
+++ src/apis/routes/recovered.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const Sequelize = require("../../db/config/connection.js");
+const Auth = require("../Middlewares/auth");
+const auth = Auth.auth;
+const sequelize = Sequelize.sequelize;
+
+router.get("/top10", auth, async (req, res) => {
+  try {
+    sequelize
+      .query(
+        "select d.country_name,c.recovered_cases from covid_infos c,countries d where c.country_code=d.id order by c.recovered_cases desc limit 10"
+      )
+      .then((data) => {
+        res.send(data);
+        res.status(200);
+      });
+  } catch (error) {
+    res.send({ error: error.message });
+    res.status(400);
+  }
+});
+
+module.exports = { router };
