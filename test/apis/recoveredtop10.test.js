f423ab74c80e2dbba8e877aea96afc03672b587e
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-23 02:33:17 +0530

    testcases for top10 countries who is having more covid recovered cases in world

--- test/apis/recoveredtop10.test.js
+++ test/apis/recoveredtop10.test.js
@@ -10,23 +10,23 @@ describe("Top 10 countries who is having highest recovered cases in world", () =
         expect(res.body).toBe([
           {
             country_name: "US",
-            no_of_cases: 283178,
+            recovered_cases: 283178,
           },
           {
             country_name: "Germany",
-            no_of_cases: 155041,
+            recovered_cases: 155041,
           },
           {
             country_name: "Spain",
-            no_of_cases: 150376,
+            recovered_cases: 150376,
           },
           {
             country_name: "Italy",
-            no_of_cases: 127326,
+            recovered_cases: 127326,
           },
           {
             country_name: "Turkey",
-            no_of_cases: 111577,
+            recovered_cases: 111577,
           },
           {
             country_name: "Brazil",
@@ -34,19 +34,19 @@ describe("Top 10 countries who is having highest recovered cases in world", () =
           },
           {
             country_name: "Iran",
-            no_of_cases: 95661,
+            recovered_cases: 95661,
           },
           {
             country_name: "Russia",
-            no_of_cases: 70209,
+            recovered_cases: 70209,
           },
           {
             country_name: "China",
-            no_of_cases: 63616,
+            recovered_cases: 63616,
           },
           {
             country_name: "France",
-            no_of_cases: 60416,
+            recovered_cases: 60416,
           },
         ]);
       });
