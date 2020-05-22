c03712914a65d810b4edee6b265624c21d004cc5
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-22 23:30:21 +0530

    testcases for admin can only upload the files

---
+++ test/apis/admin_upload.test.js
@@ -0,0 +1,35 @@
+const request = require("supertest");
+const app = require("../../src/api/Middlewares/app.js");
+const user = require("../../src/db/Models/testdb/user.js");
+
+const User = user.User;
+describe("Only admin can only insert the data into db ", async () => {
+  beforeEach(async () => {
+    User.destroy({ where: {}, truncate: true });
+  });
+
+  test("shoild upload the files ", () => {
+    request(app)
+      .post("/admin/upload")
+      .send({
+        email: "bhagya@gmail.com",
+        pass: "covid2019",
+      })
+      .expect((res) => {
+        expect(res.body).toBe("successfully uploaded the files");
+        expect(res.status).toBe(200);
+      });
+  });
+  test("it should rejects if user is not admin ", () => {
+    request(app)
+      .post("/admin/upload")
+      .send({
+        email: "user2@gmail.com",
+        pass: "user234",
+      })
+      .expect((res) => {
+        expect(res.body).toBe("You have no access to upload the files");
+        expect(res.status).toBe(403);
+      });
+  });
+});
