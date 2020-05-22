b3d27999c0d84bd72d960394c1ac37aeed22653e
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-22 23:58:13 +0530

    testcases for covid_info model

---
+++ test/models/covid_info.test.js
@@ -0,0 +1,47 @@
+const request = require("supertest");
+const covid = require("../../src/db/models/testdb/covid_info");
+const country = require("../../src/db/models/countries");
+const csv = require("csv-parser");
+const fs = require("fs");
+
+const Covid_info = covid.Covid_info;
+const Country = country.country;
+beforeEach(async function () {
+  Covid_info.sync().then(async function () {
+    Covid_info.destroy({
+      where: {},
+      truncate: true,
+    });
+  });
+});
+describe("Coutries model ", async () => {
+  test("it should insert a new record ", async () => {
+    fs.createReadStream("../../src/data/csv_files/confirmed.csv")
+      .pipe(csv())
+      .on("data", async (row) => {
+        const country = await row.Country;
+        const confirm_case = await row["5/18/20"];
+        Country.sync().then(async function () {
+          Country.findAll({
+            where: {
+              country_name: country_name,
+            },
+          }).then((data) => {
+            const country_code = data[0].id;
+            Covid_info.sync()
+              .then(async function () {
+                return Covid_info.create({
+                  confirmed_cases: country_name,
+                  country_code: country_code,
+                });
+              })
+              .then((data) => {
+                expect(data.confirmed_cases).toBe(confirm_case);
+                expect(data.country_code).toBe(country_code);
+              });
+          });
+        });
+      })
+      .on("end", () => {});
+  });
+});
