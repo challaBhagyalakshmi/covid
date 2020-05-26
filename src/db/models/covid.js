a835cea1c62929e2dfad34dc75984887dc4b4c6b
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-26 17:16:16 +0530

    covid_info model

---
+++ src/db/models/covid.js
@@ -0,0 +1,53 @@
+const Sequelize = require("sequelize");
+const conn = require("../config/connection");
+
+const sequelize = conn.sequelize;
+
+const Covid = sequelize.define("covid_infos", {
+  country_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    defaultValue: null,
+    primaryKey: false,
+  },
+  confirmed_cases: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: false,
+    defaultValue: null,
+    primaryKey: false,
+  },
+  recovered_cases: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: false,
+    defaultValue: null,
+    primaryKey: false,
+  },
+  no_of_deaths: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: false,
+    defaultValue: null,
+    primaryKey: false,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    unique: false,
+    defaultValue: null,
+    primaryKey: false,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    unique: false,
+    defaultValue: null,
+    primaryKey: false,
+  },
+});
+
+sequelize.sync();
+
+module.exports = { Covid };
