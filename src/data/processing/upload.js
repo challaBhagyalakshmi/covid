e14b313e83b98ad94af03401397f2232d9b61cfc
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-27 16:43:18 +0530

    upload data from csv to db

--- src/data/processing/upload.js
+++ src/data/processing/upload.js
@@ -1,17 +1,11 @@
-const country = require("./countries");
 const confirm = require("./update_confirm_col");
 const recover = require("./update_recovered_col");
 const deaths = require("./update_deaths_col");
 
-async function upload_data() {
-  await country.export_countries_data();
-}
-
-async function update_data() {
+async function update_col_data() {
   await confirm.export_data_csv_to_db_confirmed();
   await recover.export_data_csv_to_db_recovered();
   await deaths.export_data_csv_to_db_deaths();
 }
 
-upload_data();
-module.exports = { upload_data, update_data };
+module.exports = { update_col_data };
