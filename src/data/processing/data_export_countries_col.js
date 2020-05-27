3b956b3b9c358c16977bf85c0ed05cff4a32b588
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-27 11:16:22 +0530

    data export csv to countries col

---
+++ src/data/processing/data_export_countries_col.js
@@ -0,0 +1,42 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+const covid_info = require("../../db/Models/covid.js");
+const update_col_data = require("./upload");
+
+const Covid = covid_info.Covid;
+
+async function export_countries_data() {
+  Covid.destroy({
+    where: {},
+    truncate: true,
+  });
+  try {
+    fs.createReadStream("../csv_files/countries.csv")
+      .pipe(csv())
+      .on("data", async (row) => {
+        const data = await Object.values(row);
+        const dat = data[0];
+        await Covid.sync()
+          .then(function () {
+            return Covid.create({
+              country_name: dat,
+              confirmed_cases: 0,
+              recovered_cases: 0,
+              no_of_deaths: 0,
+            });
+          })
+          .then((data) => {
+            console.log(data);
+          });
+      })
+      .on("end", () => {
+        update_col_data.update_data();
+        console.log("csv file successfully processed");
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export_countries_data();
+module.exports = { export_countries_data };
