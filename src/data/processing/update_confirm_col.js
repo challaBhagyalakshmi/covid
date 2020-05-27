1bcef7cf4b05fdd285a9e3d48f947d45fbe32af2
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-27 11:44:51 +0530

    data updation of confirm_cases_col in covid_info model

--- src/data/processing/update_confirm_col.js
+++ src/data/processing/update_confirm_col.js
@@ -5,43 +5,50 @@ const covid_info = require("../../db/Models/covid.js");
 const Covid = covid_info.Covid;
 
 async function export_data_csv_to_db_confirmed() {
-  fs.createReadStream("../csv_files/confirmed.csv")
-    .pipe(csv())
-    .on("data", async (row) => {
-      const country = await row.Country;
-      let sum = 0;
-      update_confirmed_cases_col(country, sum);
-    })
-    .on("end", () => {
-      console.log("csv file is successfully processed");
-    });
+  try {
+    fs.createReadStream("../csv_files/confirmed.csv")
+      .pipe(csv())
+      .on("data", async (row) => {
+        const country = await row.Country;
+        let sum = 0;
+        update_confirmed_cases_col(country, sum);
+      })
+      .on("end", () => {
+        console.log("csv file is successfully processed");
+      });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function update_confirmed_cases_col(country, sum) {
-  total_no_confirm_cases = 0;
-  fs.createReadStream("../csv_files/confirmed.csv")
-    .pipe(csv())
-    .on("data", (row) => {
-      const country_name = row.Country;
-      const confirm_cases = row["5/18/20"];
-      const confirmed_cases = parseInt(confirm_cases, 10);
-      if (country.localeCompare(country_name) == 0) {
-        sum = sum + confirmed_cases;
-      }
-    })
-    .on("end", () => {
-      console.log(country + " " + sum);
-      Covid.sync().then(function () {
-        Covid.update(
-          { confirmed_cases: sum },
-          {
-            where: {
-              country_name: country,
-            },
-          }
-        );
+async function update_confirmed_cases_col(country, sum) {
+  try {
+    fs.createReadStream("../csv_files/confirmed.csv")
+      .pipe(csv())
+      .on("data", (row) => {
+        const country_name = row.Country;
+        const confirm_cases = row["5/18/20"];
+        const confirmed_cases = parseInt(confirm_cases, 10);
+        if (country.localeCompare(country_name) == 0) {
+          sum = sum + confirmed_cases;
+        }
+      })
+      .on("end", () => {
+        console.log(country + " " + sum);
+        Covid.sync().then(function () {
+          Covid.update(
+            { confirmed_cases: sum },
+            {
+              where: {
+                country_name: country,
+              },
+            }
+          );
+        });
       });
-    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = { export_data_csv_to_db_confirmed };
