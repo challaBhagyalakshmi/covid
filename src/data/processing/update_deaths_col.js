614fb2ff72bc6e59e2bffff2f2c1744803ce8c44
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-27 11:43:55 +0530

    data updation of deaths_col in covid_info model

--- src/data/processing/update_deaths_col.js
+++ src/data/processing/update_deaths_col.js
@@ -5,43 +5,50 @@ const covid_info = require("../../db/Models/covid.js");
 const Covid = covid_info.Covid;
 
 async function export_data_csv_to_db_deaths() {
-  fs.createReadStream("../csv_files/deaths.csv")
-    .pipe(csv())
-    .on("data", async (row) => {
-      const country = await row.Country;
-      let sum = 0;
-      update_deaths_cases_col(country, sum);
-    })
-    .on("end", () => {
-      console.log("csv file is successfully processed");
-    });
+  try {
+    fs.createReadStream("../csv_files/deaths.csv")
+      .pipe(csv())
+      .on("data", async (row) => {
+        const country = await row.Country;
+        let sum = 0;
+        update_deaths_cases_col(country, sum);
+      })
+      .on("end", () => {
+        console.log("csv file is successfully processed");
+      });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function update_deaths_cases_col(country, sum) {
-  total_no_confirm_cases = 0;
-  fs.createReadStream("../csv_files/deaths.csv")
-    .pipe(csv())
-    .on("data", (row) => {
-      const country_name = row.Country;
-      const deaths = row["5/18/20"];
-      const no_of_deaths = parseInt(deaths, 10);
-      if (country.localeCompare(country_name) == 0) {
-        sum = sum + no_of_deaths;
-      }
-    })
-    .on("end", () => {
-      console.log(country + " " + sum);
-      Covid.sync().then(function () {
-        Covid.update(
-          { no_of_deaths: sum },
-          {
-            where: {
-              country_name: country,
-            },
-          }
-        );
+async function update_deaths_cases_col(country, sum) {
+  try {
+    fs.createReadStream("../csv_files/deaths.csv")
+      .pipe(csv())
+      .on("data", (row) => {
+        const country_name = row.Country;
+        const deaths = row["5/18/20"];
+        const no_of_deaths = parseInt(deaths, 10);
+        if (country.localeCompare(country_name) == 0) {
+          sum = sum + no_of_deaths;
+        }
+      })
+      .on("end", () => {
+        console.log(country + " " + sum);
+        Covid.sync().then(function () {
+          Covid.update(
+            { no_of_deaths: sum },
+            {
+              where: {
+                country_name: country,
+              },
+            }
+          );
+        });
       });
-    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = { export_data_csv_to_db_deaths };
