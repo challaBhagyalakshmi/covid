1fe70d182bfd61b20338956db5abbddab9b80bba
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-27 16:46:37 +0530

    data insertion of countries_column in covid model

--- src/data/processing/data_export_countries_col.js
+++ src/data/processing/data_export_countries_col.js
@@ -1,41 +1,50 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const covid_info = require("../../db/Models/covid.js");
-const update_col_data = require("./upload");
+const update_data = require("./upload");
 
 const Covid = covid_info.Covid;
 
 async function export_countries_data() {
   try {
-    Covid.destroy({
+    await Covid.destroy({
       where: {},
       truncate: true,
     });
-    fs.createReadStream("../csv_files/countries.csv")
+    await fs
+      .createReadStream("../csv_files/countries.csv")
       .pipe(csv())
-      .on("data", async (row) => {
-        const data = await Object.values(row);
-        const dat = data[0];
-        await Covid.sync()
-          .then(function () {
-            return Covid.create({
-              country_name: dat,
-              confirmed_cases: 0,
-              recovered_cases: 0,
-              no_of_deaths: 0,
-            });
-          })
-          .then((data) => {
-            console.log(data);
-          });
+      .on("data", async function (row) {
+        const country = await row.Country_name;
+        const result = await data_insertion(country);
       })
-      .on("end", () => {
+      .on("end", async () => {
         console.log("csv file successfully processed");
+        await update_data.update_col_data();
       });
   } catch (error) {
     console.log(error);
   }
 }
 
-export_countries_data();
+async function data_insertion(country) {
+  try {
+    await Covid.sync()
+      .then(function () {
+        return Covid.create({
+          country_name: country,
+          confirmed_cases: 0,
+          recovered_cases: 0,
+          no_of_deaths: 0,
+        });
+      })
+      .then((data) => {
+        console.log(data);
+      });
+    return 1;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = { export_countries_data };
