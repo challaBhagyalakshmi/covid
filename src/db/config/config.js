48680f0f0a0ea24253b0511a77202abe9d6c4166
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-28 12:21:37 +0530

    configuration file

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -5,11 +5,8 @@ Credential = {
   dbname: process.env.DB_NAME,
   user: process.env.DB_USER,
   pass: process.env.DB_PASS,
-  port: process.env.DB_PORT,
   host: process.env.DB_HOST,
   testdb: process.env.TEST_DB,
-  admin_pass: process.env.admin_pass,
-  api_key: process.env.secret_key,
 };
 
 module.exports = { Credential };
