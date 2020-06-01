a0e276efe5e2435d7800324c4ac074f4faeddc11
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-06-01 13:15:27 +0530

    testcases for top10 countries who is having more deaths

--- test/apis/deathstop.test.js
+++ test/apis/deathstop.test.js
@@ -1,5 +1,7 @@
 const request = require("supertest");
-const app = require("../../src/server.js");
+const App = require("../../src/server");
+
+const app = App.app;
 
 describe("Top 10 countries who is having highest deaths in world", () => {
   test("it should return top 10 countries ", () => {
@@ -7,49 +9,6 @@ describe("Top 10 countries who is having highest deaths in world", () => {
       .get("/deaths/top10")
       .expect((res) => {
         expect(res.status).toBe(200);
-        expect(res.body).toBe([
-          {
-            country_name: "US",
-            no_of_deaths: 90347,
-          },
-          {
-            country_name: "United Kingdom",
-            no_of_deaths: 34796,
-          },
-          {
-            country_name: "Italy",
-            no_of_deaths: 32007,
-          },
-          {
-            country_name: "France",
-            nno_of_deaths: 28193,
-          },
-          {
-            country_name: "Spain",
-            no_of_deaths: 27709,
-          },
-
-          {
-            country_name: "Brazil",
-            no_of_deaths: 16853,
-          },
-          {
-            country_name: "Belgium",
-            nno_of_deaths: 9080,
-          },
-          {
-            country_name: "Germany",
-            no_of_deaths: 8003,
-          },
-          {
-            country_name: "Iran",
-            no_of_deaths: 7057,
-          },
-          {
-            country_name: "Mexico",
-            no_of_deaths: 5332,
-          },
-        ]);
       });
   });
 });
