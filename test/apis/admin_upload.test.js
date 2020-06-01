2517b215cbd95d41b986b29169296171d67d9008
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-06-01 11:47:00 +0530

    testcases for only admin can only upload the data into db

--- test/apis/admin_upload.test.js
+++ test/apis/admin_upload.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../../src/server");
+const bcrypt = require("bcrypt");
 const user = require("../../src/db/Models/testdb/user.js");
 
 const User = user.User;
@@ -7,13 +8,31 @@ describe("Only admin can only insert the data into db ", async () => {
   beforeEach(async () => {
     User.destroy({ where: {}, truncate: true });
   });
-
-  test("shoild upload the files ", () => {
+  test("it should create a new user ", async () => {
+    const pass = "covid2019";
+    const hashed = await bcrypt.hash(pass, 8);
+    request(app)
+      .post("/user/signup")
+      .send({
+        name: "bhagya",
+        email: "bhagya@gmail.com",
+        pass: hashed,
+      })
+      .expect((res) => {
+        expect(res.body.name).toBe("bhagya");
+        expect(res.body.email).toBe("bhagya@gmail.com");
+        expect(res.body.pass).toBe(hashed);
+        expect(res.status).toBe(200);
+      });
+  });
+  test("should upload the files ", () => {
+    const pass = "covid19";
+    const hashed = await bcrypt.hash(pass, 8);
     request(app)
       .post("/admin/upload")
       .send({
         email: "bhagya@gmail.com",
-        pass: "covid2019",
+        pass: hashed,
       })
       .expect((res) => {
         expect(res.body).toBe("successfully uploaded the files");
@@ -21,11 +40,13 @@ describe("Only admin can only insert the data into db ", async () => {
       });
   });
   test("it should rejects if user is not admin ", () => {
+    const pass = "user234";
+    const hashed = await bcrypt.hash(pass, 8);
     request(app)
       .post("/admin/upload")
       .send({
         email: "user2@gmail.com",
-        pass: "user234",
+        pass: hashed,
       })
       .expect((res) => {
         expect(res.body).toBe("You have no access to upload the files");
