96f25a4f1b1f91e3d371cc3ce24dc9a13f508553
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-06-11 10:30:17 +0530

    testcases for user model

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,8 +1,7 @@
-const connection = require("../../src/db/config/testdbconn.js");
 const user = require("../../src/db/Models/testdb/user.js");
 const bcrypt = require("bcrypt");
 const User = user.User;
-const sequelize = connection.sequelize;
+const sequelize = user.sequelize;
 
 describe("User model ", async () => {
   beforeEach(async function () {
@@ -29,19 +28,26 @@ describe("User model ", async () => {
       });
   });
 
-  it("it should fail if give invalid email ", async () => {
+  it("it should fail if invalid email ", async () => {
     const user = {
       name: "user9",
       email: "user9jdjf",
       pwd: "user999",
     };
     const hashed = await bcrypt.hash(user.pwd, 8);
-    sequelize.sync().then(function () {
-      return user.create({
-        name: user.name,
-        email: user.email,
-        pass: hashed,
+    sequelize
+      .sync()
+      .then(function () {
+        return user.create({
+          name: user.name,
+          email: user.email,
+          pass: hashed,
+        });
+      })
+      .then((data) => {
+        expect(data.name).toBe("user9");
+        expect(data.email).toBe("user9jdjf");
+        expect(data.pass).toBe(hashed);
       });
-    });
   });
 });
