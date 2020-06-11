2a75b43cf36f617de051464232c343145ccc2f0f
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-06-11 10:31:57 +0530

    testcases for top10 countries who is having more covid deaths

--- test/apis/deathstop.test.js
+++ test/apis/deathstop.test.js
@@ -9,6 +9,7 @@ describe("Top 10 countries who is having highest deaths in world", () => {
       .get("/deaths/top10")
       .expect((res) => {
         expect(res.status).toBe(200);
+        expect(res.no_of_records).toBe(10);
       });
   });
 });
