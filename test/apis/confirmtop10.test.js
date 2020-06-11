0de8ca1cb31deb6eba1e3d74e88e0b77b973f499
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-06-11 10:31:26 +0530

    testcases for top10 countries who is having more covid confirm cases

--- test/apis/confirmtop10.test.js
+++ test/apis/confirmtop10.test.js
@@ -8,6 +8,7 @@ test("it should return top 10 countries who is having more confirmed cases", () 
   request(app)
     .get("/confirmed/top10")
     .expect((res) => {
+      expect(res.no_of_records).toBe(10);
       expect(res.status).toBe(200);
     });
 });
